e769b72855320440e4935b925690fa72660f6be9
Advith Chelikani <advith.chelikani@gmail.com>
2018-01-09 20:24:26 -0800

    change to pre-event puzzles, fix profile

---
+++ _puzzles.py
@@ -0,0 +1,63 @@
+ANSWERS = {
+    "132" : "SUPREMEGREEN",
+    "839" : "BARRAMUNDI",
+    "739" : "SEASONALSOUP",
+    "324" : "STEAK",
+    "801" : "WAFFLECONES",
+    "460" : "SEACAKE",
+    "233" : "COCONUTPASTA",
+    "413" : "SPICY'CHITACO",
+    "641" : "MUSHROOMCHICKEN",
+    "957" : "BUTTERHORN",
+    "767" : "SHAWERMASALAD",
+    "240" : "ANTIOXIDANT",
+    "417" : "STEELCUTOATMEAL"
+}
+
+PUZZLES = {
+    "132" : "A Kale Venue",
+    "839" : "Bib Sheet",
+    "739" : "Catering and Dining Situation",
+    "324" : "Fantastic Feasts and Where to Find Them",
+    "801" : "Fine Dining",
+    "460" : "Food Trucks",
+    "233" : "Introduction to Macros",
+    "413" : "On the Keyboard",
+    "641" : "Round Table Talk",
+    "957" : "Sampler",
+    "767" : "Sandwich Bar",
+    "240" : "Slow Cooker",
+    "417" : "Tossed Salad"
+}
+
+POINTS = {
+    "132" : 300,
+    "839" : 100,
+    "739" : 100,
+    "324" : 100,
+    "801" : 100,
+    "460" : 100,
+    "233" : 100,
+    "413" : 100,
+    "641" : 100,
+    "957" : 100,
+    "767" : 100,
+    "240" : 100,
+    "417" : 100
+}
+
+NUTRITION_FACTS = {
+    "132" : "https://www.google.com/maps/dir/34.1431556,-118.1242755/Nekter+Juice+Bar,+345+S+Lake+Ave+%23110,+Pasadena,+CA+91101/@34.1413128,-118.1334426,16z/data=!3m2!4b1!5s0x80c2c35e59458dd7:0x7edf0a3ac168bdc5!4m17!1m6!3m5!1s0x80c2c35e5f1bb743:0x5dd65e8726213671!2sNekter+Juice+Bar!8m2!3d34.1399722!4d-118.1327204!4m9!1m1!4e1!1m5!1m1!1s0x80c2c35e5f1bb743:0x5dd65e8726213671!2m2!1d-118.1327204!2d34.1399722!3e2",
+    "839" : "https://imgur.com/a/wQvtH",
+    "739" : "https://imgur.com/a/C64IX",
+    "324" : "https://imgur.com/a/k6AEi",
+    "801" : "https://imgur.com/a/4oHFp",
+    "460" : "https://imgur.com/a/IFwMs",
+    "233" : "https://imgur.com/a/i5uoK",
+    "413" : "https://imgur.com/a/f9KJK",
+    "641" : "https://imgur.com/a/qv8HT",
+    "957" : "https://imgur.com/a/Xmawr",
+    "767" : "https://imgur.com/a/P3ba6",
+    "240" : "https://imgur.com/a/EXC3C",
+    "417" : "https://imgur.com/a/s0CPe"
+}
